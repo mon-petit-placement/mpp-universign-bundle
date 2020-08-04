901bcbf31b3693be346c9a3c2c49db0844956049
clement <clement.scherpereel@epitech.eu>
2020-08-04 17:19:04 +0200

    functionnal signature with phone number

--- XmlRpcClient/UniversignXmlRpcClient.php
+++ XmlRpcClient/UniversignXmlRpcClient.php
@@ -2,16 +2,14 @@
 
 namespace Mpp\UniversignBundle\XmlRpcClient;
 
-use Behat\Behat\HelperContainer\ContainerInterface;
+use DateTimeInterface;
 use Laminas\XmlRpc\Client;
-use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use UniversignRequest\UniversignRequest;
-
+use Laminas\XmlRpc\AbstractValue;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UniversignXmlRpcClient
 {
-
     /**
      * @var Client
      */
@@ -37,14 +35,87 @@ class UniversignXmlRpcClient
     {
         return $this->url;
     }
-    /**
-     * @return string
-     */
-    public function initiateConnection()
-    {
-        $result = $this->xmlrpcClient->call('requester.requestTransaction');
 
-        return $result;
+    /**
+     * @param OptionsResolver $resolver
+     */
+    private function configureSignatureParameters($resolver)
+    {
+        $resolver
+            ->setRequired('signers')->setAllowedTypes('signers', ['array'])->setNormalizer('signers', function (Options $option, $values) {
+                $resolver = new OptionsResolver();
+
+                $resolver
+                    ->setRequired('firstname')->setAllowedTypes('firstname', ['string'])
+                    ->setRequired('lastname')->setAllowedTypes('lastname', ['string'])
+                    ->setRequired('organization')->setAllowedTypes('organization', ['string'])
+                    ->setRequired('emailAddress')->setAllowedTypes('emailAddress', ['string'])
+                    ->setRequired('phoneNum')->setAllowedTypes('phoneNum', ['string'])
+                    ->setRequired('language')->setAllowedTypes('language', ['string'])
+                    ->setRequired('role')->setAllowedTypes('role', ['string'])
+                    ->setDefined('birthDate')->setAllowedTypes('birthDate', ['DateTime'])->setNormalizer('birthDate', function (Options $option, $value) {
+                        return $value->format(DateTimeInterface::ISO8601);
+                    })
+                    ->setRequired('universignId')->setAllowedTypes('universignId', ['string'])
+                    ->setRequired('certificateType')->setAllowedTypes('certificateType', ['string'])
+                    ->setRequired('validationSessionId')->setAllowedTypes('validationSessionId', ['string'])
+                ;
+                $resolvedParameter = [];
+
+                foreach ($values as $value) {
+                    $resolvedParameter[] = $resolver->resolve($value);
+                }
+
+                return $resolvedParameter;
+            })
+            ->setRequired('documents')->setAllowedTypes('documents', ['array'])->setNormalizer('documents', function (Options $option, $values) {
+                $resolver = new OptionsResolver();
+
+                $resolver
+                    ->setRequired('documentType')->setAllowedTypes('documentType', ['string'])
+                    ->setRequired('url')->setAllowedTypes('url', ['string'])
+                    ->setRequired('fileName')->setAllowedTypes('fileName', ['string'])
+                    ->setDefined('DocSignatureField')->setAllowedTypes('DocSignatureField', ['array'])->setNormalizer('DocSignatureField', function (Options $option, $value) {
+                        $resolver = new OptionsResolver();
+                        $resolver
+                            ->setDefined('name')->setAllowedTypes('name', ['string'])
+                            ->setRequired('page')->setAllowedTypes('page', ['int'])
+                            ->setRequired('x')->setAllowedTypes('x', ['int'])
+                            ->setRequired('y')->setAllowedTypes('y', ['int'])
+                            ->setRequired('signerIndex')->setAllowedTypes('signerIndex', ['int'])
+                        ;
+
+                        return $resolver->resolve($value);
+                    })
+                ;
+
+                $resolvedParameter = [];
+
+                foreach ($values as $value) {
+                    $resolvedParameter[] = $resolver->resolve($value);
+                }
+
+                return $resolvedParameter;
+            });
     }
 
+    /**
+     * @return string
+     * @param array<mixed> $parameters
+     */
+    public function initiateConnexion($parameters)
+    {
+        $resolver = new OptionsResolver();
+
+        $this->configureSignatureParameters($resolver);
+        $resolvedParameters = $resolver->resolve($parameters);
+
+        return $this
+            ->xmlrpcClient
+            ->call(
+                'requester.requestTransaction', [
+                     $resolvedParameters
+                ]
+            );
+    }
 }
